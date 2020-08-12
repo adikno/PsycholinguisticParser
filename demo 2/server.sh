461330cd51c64a58dca68e7af1b5ba04009fc55e
adikno <45814877+adikno@users.noreply.github.com>
2020-08-12 12:23:43 +0300

    Update server.sh

--- demo 2/server.sh
+++ demo 2/server.sh
@@ -2,5 +2,5 @@
 
 source /etc/profile
 /home/ANT.AMAZON.COM/adiknob/yapproj/src/yap/yap api
-#source ~/.profile
-#~/tmp/PsychFeatures/yapproj/src/yap/yap api
+#source /home/natalie/.profile
+#/home/natalie/tmp/PsychFeatures/yapproj/src/yap/yap api
