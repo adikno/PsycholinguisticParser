7057b3c247e662baaa72cc7ad8c15d4eaa05f6c0
adikno <45814877+adikno@users.noreply.github.com>
2020-08-15 22:56:11 +0300

    Update SpringBootUploadFilesApplication.java

--- demo 2/src/main/java/com/example/demo/SpringBootUploadFilesApplication.java
+++ demo 2/src/main/java/com/example/demo/SpringBootUploadFilesApplication.java
@@ -4,7 +4,6 @@ package com.example.demo;
 import javax.annotation.Resource;
 
 import com.example.demo.server.Client;
-import com.example.demo.server.Server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +14,6 @@ import com.example.demo.service.FilesStorageService;
 public class SpringBootUploadFilesApplication implements CommandLineRunner {
     @Resource
     FilesStorageService storageService;
-    private Server server;
     private Client client;
 
     public static void main(String[] args) {
@@ -25,9 +23,6 @@ public class SpringBootUploadFilesApplication implements CommandLineRunner {
     @Override
     public void run(String... arg) throws Exception {
         storageService.deleteAll();
-        this.server = new Server();
-        System.out.println("about to connect to the server");
-        server.start();
         this.client = new Client();
         System.out.println("about to launch the client");
         client.start();
