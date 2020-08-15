e8f2f19d07621584b812cfb98fe024df720e6faa
adikno <45814877+adikno@users.noreply.github.com>
2020-08-15 22:57:48 +0300

    Update server.sh

--- demo 2/server.sh
+++ demo 2/server.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-source /etc/profile
-/home/ANT.AMAZON.COM/adiknob/yapproj/src/yap/yap api
 #source /home/natalie/.profile
-#/home/natalie/tmp/PsychFeatures/yapproj/src/yap/yap api
+source /etc/profile
+
+while true; do
+        echo "uploading YAP"
+        /home/ANT.AMAZON.COM/adiknob/yapproj/src/yap/yap api > /dev/null 2>&1 &
+        #/home/natalie/tmp/PsychFeatures/yapproj/src/yap/yap api > /dev/null 2>&1 &
+        process_id=$!
+        sleep 35
+        echo "YAP is supposed to be up"
+        sleep 120
+        echo "killing YAP"
+        kill $process_id
+done
