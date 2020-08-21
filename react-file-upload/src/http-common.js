917f475dbbeb6232d42574ebafbfaa4973dcf2eb
adikno <45814877+adikno@users.noreply.github.com>
2020-08-21 15:51:13 +0300

    Update http-common.js

--- react-file-upload/src/http-common.js
+++ react-file-upload/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
-  //baseURL: "http://132.70.30.35:80",
+  //baseURL: "http://localhost:8080",
+  baseURL: "http://132.70.30.35:80",
   headers: {
     "Content-type": "application/json"
   }
